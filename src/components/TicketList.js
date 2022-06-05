e0cff937c72a1c7e1ee8628428506a42131dd221
Nick Sullivan <nicholassull@gmail.com>
2022-06-05 14:24:44 -0700

    update TicketList.js to work with redux

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -6,21 +6,22 @@ function TicketList(props){
   return (
     <React.Fragment>
       <hr/>
-      {/* {props.ticketList.map((ticket) =>
+      {Object.values(props.ticketList).map((ticket) =>
         <Ticket
           whenTicketClicked = { props.onTicketSelection }
           names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
+          formattedWaitTime={ticket.formattedWaitTime}
           id={ticket.id}
           key={ticket.id}/>
-      )} */}
+      )}
     </React.Fragment>
   );
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array,
+  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
