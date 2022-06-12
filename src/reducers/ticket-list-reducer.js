f824708086389e408e6ffef63f7b5bb74f634221
Nick Sullivan <nicholassull@gmail.com>
2022-06-12 12:01:28 -0700

    add Update Time case to ticket-list-reducer

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,7 +1,7 @@
 import * as c from './../actions/ActionTypes.js'
 
 export default (state = {}, action) => {
-  const {names, location, issue, id} = action;
+  const {names, location, issue, id, formattedWaitTime, timeOpen} = action;
   switch (action.type) {
     case c.ADD_TICKET:
       //Object.assign takes 3 arguments: 1st must be an empty object {} so it can mutate that instead of the state passed in, 2nd is the object that will be cloned into the new object, 3rd is the change that will be made to the new copy (in this case the new ticket that will be added). 
@@ -18,6 +18,14 @@ export default (state = {}, action) => {
       let newState = {...state};
       delete newState[id];
       return newState;
+    case c.UPDATE_TIME:
+      //Note - the formattedWaitTime is an object with a key-value pair.
+      const newTicket = Object.assign({}, state[id], {formattedWaitTime});
+      //The old ticket is replaced by the new ticket with waitTime.
+      const updatedState = Object.assign({}, state, {
+        [id]: newTicket
+      });
+      return updatedState;
     default:
       return state;
   }
