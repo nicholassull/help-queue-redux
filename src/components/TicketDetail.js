f85a0a68a468c4bfc7eedc47a426d44d76eb1e74
Nick Sullivan <nicholassull@gmail.com>
2022-06-12 12:41:40 -0700

    update TicketDetails to include wait time

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -9,6 +9,7 @@ function TicketDetail(props){
       <h1>Ticket Detail</h1>
       <h3>{ticket.location} - {ticket.names}</h3>
       <p><em>{ticket.issue}</em></p>
+      <p><em>{ticket.formattedWaitTime}</em></p>
       <button onClick={ props.onClickingEdit }>Update Ticket</button>
       <button onClick={()=> onClickingDelete(ticket.id) }>Close Ticket</button>
       <hr/>
