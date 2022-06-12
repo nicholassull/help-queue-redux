0178b4aba1d3460f82aa2925a3858282d65d9e19
Nick Sullivan <nicholassull@gmail.com>
2022-06-12 10:58:01 -0700

    add selected-ticket-reducer.js

--- src/reducers/selected-ticket-reducer.js
+++ src/reducers/selected-ticket-reducer.js
@@ -1,1 +1,10 @@
 import * as c from './../actions/ActionTypes.js'
+
+export default (state = null, action) => {
+  switch (action.type) {
+    case c.SELECT_TICKET:
+      return action.id;
+    default:
+      return state;
+  }
+}
